8b5c43d4e20c37bc50ef4dc8a40bc87d0069bf8e
Lennart Koopmann <lennart@scopeport.org>
2010-08-10 00:47:53 +0200

    web socket m-m-m-m-aaaagic

--- config/initializers/websocketserver.rb
+++ config/initializers/websocketserver.rb
@@ -1,6 +1,6 @@
 require "#{RAILS_ROOT}/lib/graylog2-websocket.rb"
 
-Thread.new {
+Thread.new do
   server = Graylog2::WebSocketServer.new
   server.launch
-}
+end
--- lib/graylog2-websocket.rb
+++ lib/graylog2-websocket.rb
@@ -13,23 +13,38 @@ module Graylog2
     end
 
     def handle msg
-      request = ActiveSupport::JSON.decode(msg)
-      case request["type"]
-        when 'status':
-          # okay/problems
-        when 'lastmessage':
-          message = Message.all_with_blacklist(0, 1)
-          message = message[0]
-          glsend message.message
-        when 'messagecount':
-          glsend "maximum: #{request["maximum"]}"
-        when 'lastmessages':
+      begin
+        request = ActiveSupport::JSON.decode msg
+        case request["type"]
+          when "status":
+            state = Message.count_of_last_minutes(request["interval"]) > Setting.get_message_max_count(request["maximum"])
+            if state == true
+              glsend "0", "status"
+            else
+              glsend "1", "status"
+            end
+
+          when "lastmessage":
+            message = Message.all_with_blacklist 0, 1
+            message = message[0]
+            glsend "#{Time.at(message.created_at).to_s}: #{message.message}", "lastmessage"
+
+          when "messagecount":
+            count = Message.count_of_last_minutes request["interval"]
+            glsend count.to_s, "messagecount"
+
+          when "lastmessages":
+        end
+      rescue
+        glsend "error"
       end
     end
 
-    def glsend msg
-      puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA SENDING: #{msg}"
-      @socket.send msg
+    def glsend msg, type = "system"
+      begin
+        @socket.send ActiveSupport::JSON.encode({ "m" => msg, "t" => type })
+      rescue
+      end
     end
   end
 
